94094410b5cdfe8400a5ba52b061234e43be541f
Filipe sacchet kaizer <filipe09zzaa@gmail.com>
2023-11-22 09:13:57 -0300

    Menu básico adicionado. Agora ele consegue abrir a screen responsável pelo jogo.

--- core/src/com/mygdx/game/Screens/MenuPrincipal.java
+++ core/src/com/mygdx/game/Screens/MenuPrincipal.java
@@ -2,13 +2,25 @@ package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.Cenas.Hud;
@@ -16,18 +28,61 @@ import com.mygdx.game.MyGdxGame;
 
 public class MenuPrincipal implements Screen {
     private MyGdxGame jogo;
+    private SpriteBatch batch;
+    private Stage stage;
+    private TextButton btIniciar;
+    private TextButton btPlacar;
+    Texture background;
     private OrthographicCamera camera;
     private Viewport ViewPortCamera;
 
     public MenuPrincipal(MyGdxGame jogo) {
+        batch = new SpriteBatch();
         this.jogo = jogo;
         //Cria sistema de camera e resize
         camera = new OrthographicCamera();
         ViewPortCamera = new StretchViewport(800, 480, camera);
+
+        background = new Texture(Gdx.files.internal("MenuPrincipal/FundoIA.png"));
+
     }
     @Override
     public void show() {
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
 
+        TextButton.TextButtonStyle estilo = new TextButton.TextButtonStyle();
+        estilo.font = new BitmapFont();
+        estilo.fontColor = Color.CYAN;
+        estilo.up = createButtonBackground(Color.DARK_GRAY);
+        estilo.down = createButtonBackground(Color.GRAY);
+
+        // Cria o botao sde inicio
+        btIniciar = new TextButton("Iniciar", estilo);
+        btPlacar = new TextButton("Placar", estilo);
+        btIniciar.setPosition((float)Gdx.graphics.getWidth() / 2, (float)Gdx.graphics.getHeight() / 2 - 40);
+        btIniciar.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y){
+                jogo.setScreen(new PlayGame(jogo));
+            }
+        });
+        btPlacar.setPosition((float) Gdx.graphics.getWidth() / 2, (float)Gdx.graphics.getHeight() / 2);
+
+        stage.addActor(btIniciar);
+        stage.addActor(btPlacar);
+
+    }
+
+    private Drawable createButtonBackground(Color color) {
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(color);
+        pixmap.fill();
+
+        // Crie um drawable com base na pixmap
+        Texture texture = new Texture(pixmap);
+        pixmap.dispose(); // Libere os recursos da pixmap
+
+        return new TextureRegionDrawable(new TextureRegion(texture));
     }
 
     @Override
@@ -35,6 +90,12 @@ public class MenuPrincipal implements Screen {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        stage.getBatch().begin();
+        stage.getBatch().draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        stage.getBatch().end();
+
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
     }
 
     @Override
@@ -60,6 +121,8 @@ public class MenuPrincipal implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
+        background.dispose();
+        batch.dispose();
     }
 }
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
+		config.setWindowedMode(800, 480);
 		config.setTitle("Novo");
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
