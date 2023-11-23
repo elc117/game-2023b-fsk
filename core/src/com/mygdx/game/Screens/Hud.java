edf1f56422a393d3166e3c154cf6b7cccf3bc93d
jeansteilmann <jean.steilmann@acad.ufsm.br>
2023-11-23 14:57:04 -0300

    Adição de alguns métodos na classe Hud

--- core/src/com/mygdx/game/Screens/Hud.java
+++ core/src/com/mygdx/game/Screens/Hud.java
@@ -11,7 +11,10 @@ import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-
+import java.io.FileWriter;
+import java.io.IOException;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.TimeUtils;
 import java.awt.*;
 
 public class Hud {
@@ -44,4 +47,57 @@ public class Hud {
         bfTempo.draw(batch, "Tempo: "+this.tempoCounter+"s", Gdx.graphics.getWidth() - 100, Gdx.graphics.getHeight() - 1);
     }
 
+    //Getters and Setters
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+
+    public int getPontos() {
+        return this.pontos;
+    }
+
+    public void setAcertos(int acertos) {
+        this.acertos = acertos;
+    }
+
+    public int getAcertos() {
+        return this.acertos;
+    }
+
+    public void setTempoCounter(long tempoCounter) {
+        this.tempoCounter = tempoCounter;
+    }
+
+    public long getTempoCounter() {
+        return this.tempoCounter;
+    }
+
+    public void salvarLeaderboard(String nome) {
+        FileHandle file = Gdx.files.local("LeaderBoard.csv");
+        // Verifique se o arquivo já existe
+        boolean fileExists = file.exists();
+
+        // Use FileWriter para escrever no arquivo
+        try {
+            FileWriter fileWriter = new FileWriter(file.file(), true); // O segundo parâmetro true indica que estamos anexando ao arquivo
+
+            // Se o arquivo não existia antes, adicione um cabeçalho
+            if (!fileExists) {
+                fileWriter.append("Nome,Tempo,Pontos,Acertos\n");
+            }
+            // Adicione os dados ao arquivo
+            fileWriter.append(nome)
+                    .append(',')
+                    .append(String.valueOf(tempoCounter))
+                    .append(',')
+                    .append(String.valueOf(pontos))
+                    .append(',')
+                    .append(String.valueOf(acertos))
+                    .append('\n');
+            // Feche o FileWriter
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
